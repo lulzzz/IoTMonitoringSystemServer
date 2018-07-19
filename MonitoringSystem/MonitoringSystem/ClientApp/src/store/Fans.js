92134d1edd4cb4c05de8b1a29ac0de3816f5a29f
Đàm Đức Duy <damducduy.it@gmail.com>
2018-07-19 14:38:21 +0700

    update fan status

--- MonitoringSystem/MonitoringSystem/ClientApp/src/store/Fans.js
+++ MonitoringSystem/MonitoringSystem/ClientApp/src/store/Fans.js
@@ -20,6 +20,40 @@ export const actionCreators = {
       fans
     });
     return;
+  },
+
+  updateFans: (isLoaded, fan) => async (dispatch, getState) => {
+    console.log("updateFans");
+    console.log(isLoaded);
+    console.log(getState().fans.isLoaded);
+    if (isLoaded === getState().fans.isLoaded) {
+      // Don't issue a duplicate request (we already have or are loading the requested data)
+      return;
+    }
+    console.log("updateFans2");
+    dispatch({ type: requestFansType, isLoaded });
+    console.log("updateFans3");
+    const url = `api/fans/update/` + fan.fanId;
+
+    const response = await fetch(url, {
+      method: "PUT",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(fan)
+    });
+    console.log(response);
+
+    const url2 = `api/fans/getall`;
+    const response2 = await fetch(url);
+    const fans = await response.json();
+    dispatch({
+      type: receiveFansType,
+      isLoaded,
+      fans
+    });
+    return;
   }
 };
 
