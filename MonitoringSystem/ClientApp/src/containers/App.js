370799040093dc7e87daa5cae90681f4085d3b71
Đàm Đức Duy <damducduy.it@gmail.com>
2018-07-25 13:40:26 +0700

    add admin page

--- MonitoringSystem/ClientApp/src/containers/App.js
+++ MonitoringSystem/ClientApp/src/containers/App.js
@@ -1,19 +1,21 @@
-﻿import React from 'react';
-import { Route } from 'react-router';
-import Layout from '../components/Layout';
-import Dashboard from '../components/Dashboard';
-import Fan from '../components/Fan';
-import Counter from '../components/Counter';
-import FetchData from '../components/FetchData';
-import AsyncFans from '../containers/AsyncFans'
+﻿import React from "react";
+import { Route } from "react-router";
+import Layout from "../components/Layout";
+import Dashboard from "../components/Dashboard";
+import Fan from "../components/Fan";
+import Counter from "../components/Counter";
+import FetchData from "../components/FetchData";
+import AsyncFans from "../containers/AsyncFans";
+import Admin from "../components/Admin/Admin";
 
 export default () => (
   <Layout>
     {/* <Route exact path='/' component={Home} /> */}
-    <Route exact path='/' component={Dashboard} />
-    <Route path='/fans' component={AsyncFans} />
-    <Route path='/fan' component={Fan} />
-    <Route path='/counter' component={Counter} />
-    <Route path='/fetchdata/:startDateIndex?' component={FetchData} />
+    <Route exact path="/" component={Dashboard} />
+    <Route path="/fans" component={AsyncFans} />
+    <Route path="/fan" component={Fan} />
+    <Route path="/counter" component={Counter} />
+    <Route path="/admin" component={Admin} />
+    <Route path="/fetchdata/:startDateIndex?" component={FetchData} />
   </Layout>
 );
